b5b10c290ed7ec301ec553efce26c5267ea17068
Deshan <deshanekanayake2003@gmail.com>
2026-05-12 22:25:32 +0100

    Add CSRF trusted origins

--- my_project/settings.py
+++ my_project/settings.py
@@ -9,6 +9,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-8ef73.up.railway.app']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
